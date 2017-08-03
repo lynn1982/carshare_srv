f1c40df36efd9dc8bc5eb79b8346ca03f554d55d
lynn1982 <snowdream_00@outlook.com>
2017-08-03 16:07:26 +0800

    sequelize for user

---
+++ config.js
@@ -0,0 +1,22 @@
+/**
+ * config
+ */
+
+var config = {
+    
+    host: 'localhost',
+
+    database: 'car_share',
+
+    db_username: 'root',
+
+    db_passwd: '111111',
+
+    redis_host: '127.0.0.1',
+    redis_port: 6379,
+    redis_db: 0,
+    redis_passwd: '',
+
+};
+
+module.exports = config;
---
+++ model/user.js
@@ -0,0 +1,31 @@
+
+var Sequelize = require('sequelize');
+var sequelize = require('../lib/mysql');
+
+var User = sequelize.define('user', {
+
+    id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true, unique: true},
+
+    wx_name: {type: Sequelize.STRING},
+
+    wx_id: {type: Sequelize.STRING},
+
+    passwd: {type: Sequelize.STRING},
+
+    phone_num: {type: Sequelize.STRING},
+
+    car_license: {type: Sequelize.STRING},
+
+    score: {type: Sequelize.INTEGER},
+});
+
+var user = User.sync({force: false});
+
+exports.addUser = function(user) {
+    return User.create({
+        wx_name: user.wx_name,
+        wx_id: user.wx_id,
+        phone_num: user.phone_num,
+        passwd: user.passwd,
+    });
+};
