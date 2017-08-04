3a4ffc51b7a2f4b30923ca53bb2da27404a64e8d
lynn1982 <snowdream_00@outlook.com>
2017-08-04 09:33:49 +0800

    add control

---
+++ control/manage.js
@@ -0,0 +1,8 @@
+
+
+function message_handle(req, res) {
+
+}
+
+
+module.exports.message_handle = message_handle;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var app = require('../control/app');
+var dev = require('../control/dev');
+var manage = require('../control/manage');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/parking_app', function(req, res, next) {
+    app.message_handle(req, res);
+});
+
+router.post('/parking_dev', function(req, res, next) {
+    dev.message_handle(req, res);
+});
+
+router.post('/parking_manage', function(req, res, next) {
+    manage.message_handle(req, res);
+});
+
 
 module.exports = router;
