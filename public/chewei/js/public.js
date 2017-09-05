e0ce024047ccfb019b2a6125c43de696209592f0
gjz22cn <gjz22cn@hotmail.com>
2017-09-05 12:21:39 +0800

    update for changshang,xiaoqu,user pages

--- public/chewei/js/public.js
+++ public/chewei/js/public.js
@@ -1,62 +1,50 @@
 backBtn();
 setTitleHeader();
-//bigBackBtn();//设置大返回按钮
-
-
 
 function backBtn(){
 	$(".backBtn").on("click",function(){
 		history.back();
-		
-//		alert("返回");
 		//返回后刷新页面
 		location.href=document.referrer;
 	});
 }
 
-
-
 function setTitleHeader() {
-	
-	var xiaoquManager = '<a href="editXiaoqu.html" class="selectTitle" target="iframe1">/ 编辑小区信息</a>';
-	
-	//插件
-	var addPlugin =
-		'<a href="addPlugin.html" class="selectTitle" target="iframe1">/ 添加插件</a>';
-	var pluginVersion =
-		'<a href="pluginVersion.html" class="selectTitle" target="iframe1">/ 插件版本</a>';
-	var addPluginVersion =
-		'<a href="pluginVersion.html" class="selectTitle" target="iframe1">/ 插件版本</a>'+
-		'<a href="addPluginVersion.html" class="selectTitle" target="iframe1">/ 添加版本</a>';
-	//设备
-	var addDevice = '<a href="addDevice.html" class="selectTitle" target="iframe1">/ 添加设备</a>';
-	var deviceDetail = '<a href="deviceDetail.html" class="selectTitle" target="iframe1">/ 设备详情</a>';
-	var addFirmwareVersion = '<a href="deviceDetail.html" class="selectTitle" target="iframe1">/ 设备详情</a>'+
-                       '<a href="addFirmwareVersion.html" class="selectTitle" target="iframe1">/ 添加固件版本</a>';
-	var addCode1 = '<a href="addCode.html" class="selectTitle" target="iframe1">/ 添加红外码库</a>';
-	var addLircDevice = '<a href="addLircDevice.html" class="selectTitle" target="iframe1">/ 添加红外设备</a>';
-	var lircDeviceDetail = '<a href="lircDeviceDetail.html" class="selectTitle" target="iframe1">/ 红外设备详情</a>';
-     
-     var  router_retransmission = '<a href="router_retransmission.html" class="selectTitle" target="iframe1">/ 直播资源转发情况</a>';
-     
-     var  router_currentresourceSmall = '<a href="router_currentresourceSmall.html" class="selectTitle" target="iframe1">/ 路由器当前直播资源</a>';
-     
-     var  router_historyresourceSmall = '<a href="router_historyresourceSmall.html" class="selectTitle" target="iframe1">/ 路由器历史直播资源</a>';
-     
-     
-     
+
+    var xiaoquManager = '<a href="editXiaoqu.html" class="selectTitle" target="iframe1">/ 编辑小区信息</a>';
+
+    //插件
+    var addPlugin = '<a href="addPlugin.html" class="selectTitle" target="iframe1">/ 添加插件</a>';
+    var pluginVersion = '<a href="pluginVersion.html" class="selectTitle" target="iframe1">/ 插件版本</a>';
+    var addPluginVersion = '<a href="pluginVersion.html" class="selectTitle" target="iframe1">/ 插件版本</a>' + 
+        '<a href="addPluginVersion.html" class="selectTitle" target="iframe1">/ 添加版本</a>';
+
+    //设备
+    var addDevice = '<a href="addDevice.html" class="selectTitle" target="iframe1">/ 添加设备</a>';
+    var deviceDetail = '<a href="deviceDetail.html" class="selectTitle" target="iframe1">/ 设备详情</a>';
+    var addFirmwareVersion = '<a href="deviceDetail.html" class="selectTitle" target="iframe1">/ 设备详情</a>'+
+        '<a href="addFirmwareVersion.html" class="selectTitle" target="iframe1">/ 添加固件版本</a>';
+    var addCode1 = '<a href="addCode.html" class="selectTitle" target="iframe1">/ 添加红外码库</a>';
+    var addLircDevice = '<a href="addLircDevice.html" class="selectTitle" target="iframe1">/ 添加红外设备</a>';
+    var lircDeviceDetail = '<a href="lircDeviceDetail.html" class="selectTitle" target="iframe1">/ 红外设备详情</a>';
+    var router_retransmission = '<a href="router_retransmission.html" class="selectTitle" target="iframe1">/ 直播资源转发情况</a>';
+    var router_currentresourceSmall = '<a href="router_currentresourceSmall.html" class="selectTitle" target="iframe1">/ 路由器当前直播资源</a>';
+    var router_historyresourceSmall = '<a href="router_historyresourceSmall.html" class="selectTitle" target="iframe1">/ 路由器历史直播资源</a>';
+
+
+
     var titleJson = {xiaoquManager:xiaoquManager,lircDeviceDetail:lircDeviceDetail,addLircDevice:addLircDevice,"addCode1":addCode1,"addPlugin":addPlugin,"pluginVersion":pluginVersion,"addPluginVersion":addPluginVersion,"addDevice":addDevice,"deviceDetail":deviceDetail,"addFirmwareVersion":addFirmwareVersion,"router_retransmission":router_retransmission,"router_currentresourceSmall":router_currentresourceSmall,"router_historyresourceSmall":router_historyresourceSmall};
-      //删除
-     $(".titleContainer",parent.document).find(".selectTitle").not(":first").remove();
+    //删除
+    $(".titleContainer",parent.document).find(".selectTitle").not(":first").remove();
     var fileName = $("#fileName").attr("fileNam");
     var titleEle = titleJson[fileName];
-//  alert(titleEle);
-if(titleEle){
-	
-     //添加
-    $(".titleContainer",parent.document).append(titleEle);
-}
-   
+    //  alert(titleEle);
+    if(titleEle){
+
+        //添加
+        $(".titleContainer",parent.document).append(titleEle);
+    }
+
 }
 
 
@@ -64,9 +52,9 @@ if(titleEle){
 var urlArr = ["", "/firmware/status", "/firmware/queryAllnow", "/firmware/queryById", "/firmware/queryAllhistory", "/firmware/queryAllroute", "/firmware/querynowmsg", "/firmware/queryhismsg", "/firmware/totalBytesBytime", "/firmware/totalsumByweek","/firmware/totalBytesBydeviceId"];
 
 function myajax(urli,type,data,succallback){
-	// alert(urlArr[urli])
+    // alert(urlArr[urli])
 
-	$.ajax({
+    $.ajax({
         type: type,
         url: urlArr[urli],
         async: true,
@@ -74,7 +62,43 @@ function myajax(urli,type,data,succallback){
         cache: false,
         success: succallback,
         error: function () {
-//       alert("加载失败!");
+            //       alert("加载失败!");
         }
     });//ajax
+}
+
+function priAjaxGet(path, data, successfunc, errorfunc) {
+    var url = window.location.protocol+"//"+window.location.host;
+    $.ajax({
+        type: "get",
+        url: url+path,
+        timeout: 2000,
+        data: data,
+        dataType: "json",
+        success: function(ret) {
+            successfunc(ret);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            errorfunc();
+        }
+    });
+}
+
+function priAjaxPost(path, query, successfunc, errorfunc) {
+    var url = window.location.protocol+"//"+window.location.host;
+    $.ajax({
+        type: "post",
+        url: url+path,
+        timeout: 2000,  //3s
+        async: true,    //异步
+        data: JSON.stringify(query),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(ret) {
+            successfunc(ret);
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            errorfunc();
+        }
+    });
 }
